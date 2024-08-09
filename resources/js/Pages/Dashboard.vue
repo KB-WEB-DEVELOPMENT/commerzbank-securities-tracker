c81e4b55f7a5f621950a78a39fa6f64ff651bf87
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:43:17 +0200

    Add files via upload

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+    
+		<template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+        
+		<div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+					<span>Welcome, {{ viewModel.user.name }},</span>
+					<br/>
+					<span>Access your security accounts ids: <a :href="accounts">here</a></span>
+                </div>
+            </div>
+        </div>
+    
+	</BreezeAuthenticatedLayout>
+	
+</template>
+
+<script>
+
+	import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+	import { Head } from '@inertiajs/inertia-vue3';
+
+	export default {
+		components: {
+			BreezeAuthenticatedLayout,
+			Head,
+		},
+		props: {
+			viewModel: {
+				type: Object,
+				required: true,
+			},
+		},
+	}
+
+</script>
