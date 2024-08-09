1b894b1c027441ff615026e09ab3f621009c1e50
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:27:16 +0200

    Add files via upload

---
+++ database/migrations/2024_07_13_193482_create_portfolios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            
+			$table->id();
+			$table->string('creationDay');
+			$table->string('effectiveDate');
+			$table->decimal('totalValue_amount',total:12,places:2);
+			$table->string('totalValue_currency');
+			$table->foreign('account_id')->references('securitiesAccountId')->on('accounts');
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
