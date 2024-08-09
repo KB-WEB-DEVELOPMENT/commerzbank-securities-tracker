4501120a85d82607ca37ec93f3adaf3ea08b500b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:16:55 +0200

    Update 2024_07_13_583194_create_positions_table.php

--- database/migrations/2024_07_13_583194_create_positions_table.php
+++ database/migrations/2024_07_13_583194_create_positions_table.php
@@ -15,32 +15,32 @@ return new class extends Migration
     {
         Schema::create('positions', function (Blueprint $table) {
             $table->id();
-			$table->decimal('accruedInterest_amount',total:12,places:2);
-			$table->string('accruedInterest_currency');
-			$table->decimal('currentPrice_amount',total:12,places:2);
-			$table->string('currentPrice_unit');
-			$table->decimal('exchangeRate',total:2,places:1)->default(1.0);
-			$table->string('exchangeRateDate');	
-			$table->string('quoteDate');
-			$table->decimal('initialPrice_amount',total:12,places:2);			
-			$table->string('initialPrice_unit');
-			$table->decimal('initialExchangeRate',total:2,places:1)->default(1.0);			
-			$table->string('lastTradeDate');
-			$table->decimal('payedAccruedInterest_amount',total:12,places:2);	
-			$table->string('payedAccruedInterest_currency');
-			$table->decimal('payout_amount',total:12,places:2);	
-			$table->string('payout_currency');			
-			$table->string('positionId');			
-			$table->decimal('quantity_amount',total:12,places:2);	
-			$table->string('quantity_unit');
-			$table->string('masterdata_position_isin');
-			$table->string('masterdata_position_wkn');
-			$table->string('masterdata_position_name');
-			$table->string('masterdata_position_notationType');
-			$table->string('masterdata_currency');
-			$table->string('masterdata_maturityDate');
-			$table->string('masterdata_vote');
-			$table->foreignId('portfolio_id')->constrained();
+	    $table->decimal('accruedInterest_amount',total:12,places:2);
+	    $table->string('accruedInterest_currency');
+	    $table->decimal('currentPrice_amount',total:12,places:2);
+	    $table->string('currentPrice_unit');
+	    $table->decimal('exchangeRate',total:2,places:1)->default(1.0);
+	    $table->string('exchangeRateDate');	
+	    $table->string('quoteDate');
+	    $table->decimal('initialPrice_amount',total:12,places:2);			
+	    $table->string('initialPrice_unit');
+	    $table->decimal('initialExchangeRate',total:2,places:1)->default(1.0);			
+	    $table->string('lastTradeDate');
+	    $table->decimal('payedAccruedInterest_amount',total:12,places:2);	
+	    $table->string('payedAccruedInterest_currency');
+	    $table->decimal('payout_amount',total:12,places:2);	
+	    $table->string('payout_currency');			
+	    $table->string('positionId');			
+	    $table->decimal('quantity_amount',total:12,places:2);	
+	    $table->string('quantity_unit');
+	    $table->string('masterdata_position_isin');
+	    $table->string('masterdata_position_wkn');
+	    $table->string('masterdata_position_name');
+	    $table->string('masterdata_position_notationType');
+	    $table->string('masterdata_currency');
+	    $table->string('masterdata_maturityDate');
+	    $table->string('masterdata_vote');
+	    $table->foreignId('portfolio_id')->constrained();
         });
     }
 
