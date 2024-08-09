2f459c341c142b3eb509a8c80e4f678d7e8a39f4
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:22:14 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,32 +12,35 @@ use Illuminate\Database\Seeder;
  
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeders.
-     */
-    public function run(): void
-    {
-		$this->call([
-			  
-			$user = User::factory()->make([
-				'email' => 'demo@commerzbank.com',
-				'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-			]),
-			$account = Account::factory()->make([
-				'user_id' => $user->id,
-			]),
-			$portfolio = Portfolio::factory()->make([
-				'account_id' => $account->securitiesAccountId,
-			]),
-			$position = Position::factory()->make([
-				'portfolio_id' => $portfolio->id,
-			])->count(5),
-			$transaction = Transaction::factory()->make([
-				'portfolio_id' => $account->securitiesAccountId,
+  /**
+  * Run the database seeders.
+  */
+   public function run(): void
+   {
+      $this->call([
+		  
+	  $user = User::factory()->make([
+			 'email' => 'demo@commerzbank.com',
+			 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+		  ]),
+		  
+	  $account = Account::factory()->make([
+		         'user_id' => $user->id,
+		     ]),
+			
+	  $portfolio = Portfolio::factory()->make([
+		         'account_id' => $account->securitiesAccountId,
+	  	       ]),
+			
+	  $position = Position::factory()->make([
+		         'portfolio_id' => $portfolio->id,
+		      ])->count(5),
+		  
+	  $transaction = Transaction::factory()->make([
+			    'portfolio_id' => $account->securitiesAccountId,
 			])->count(5),
 
-			// optionally: UserSeeder::class, AccountSeeder::class, PortfolioSeeder::class, PositionSeeder::class, TransactionSeeder::class,
-		]);
-
-    }
+	 // optionally: UserSeeder::class, AccountSeeder::class, PortfolioSeeder::class, PositionSeeder::class, TransactionSeeder::class,
+    ]);
+  }
 }
