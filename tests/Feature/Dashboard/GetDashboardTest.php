9b1e6c19892e1ebe9191f796ea8b32df174df59d
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 12:00:47 +0200

    Create GetDashboardTest.php

---
+++ tests/Feature/Dashboard/GetDashboardTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature\Dashboard;
+
+use App\Models\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class GetDashboardTest
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_should_load_the_dashboard():void
+    {
+		$user = User::factory()->create();
+		
+		$this->actingAs($user);	
+								
+		$name = $user->name; 
+
+		$welcome_url = 'Welcome, ' . $name;
+				
+		$response = $this->get('/dashboard');
+     
+        $response->assertSee($welcome_url)->assertSee('Access your security accounts ids');    
+    }
+}
