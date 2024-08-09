632083c045bc64f08b6ac616f4407cbba7829571
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 13:35:03 +0200

    Update TransactionsCollection.php

--- app/Collections/TransactionsCollection.php
+++ app/Collections/TransactionsCollection.php
@@ -8,55 +8,55 @@ use Illuminate\Database\Eloquent\Collection;
 
 class TransactionsCollection extends Collection
 {
-    // for demonstration purposes, we assume all amounts are expressed in the same currency
-	public function sortTransactionsByAccruedInterestAmountDesc(string $securitiesAccountId) : ?Collection
-    {		
-		$accountId = AccountId::from($securitiesAccountId);
+  // for demonstration purposes, we assume all amounts are expressed in the same currency
+  public function sortTransactionsByAccruedInterestAmountDesc(string $securitiesAccountId) : ?Collection
+  {		
+      $accountId = AccountId::from($securitiesAccountId);
 		
-		$securitiesAccountId = $accountId->securitiesAccountId();
+      $securitiesAccountId = $accountId->securitiesAccountId();
 		
-		$transactionsCollection = Transaction::where('account_id',$securitiesAccountId)->get();
+      $transactionsCollection = Transaction::where('account_id',$securitiesAccountId)->get();
 		
-		if ($transactionsCollection->isEmpty()) {
-				return null;
-		}
-		
-		if ($transactionsCollection->count() == 1) {
-				return $transactionsCollection;
-		}	
+      if ($transactionsCollection->isEmpty()) {
+      	return null;
+      }
+	  
+      if ($transactionsCollection->count() == 1) {
+        return $transactionsCollection;
+      }	
 
-		$sorted = 	$transactionsCollection->sortByDesc([
-						fn (array $transacArray1, array $transacArray2) 
-							=> $transacArray1['accruedInterest_amount'] <=> $transacArray2['accruedInterest_amount']
-					]);		
-		
-		return $sorted->values()->all();
-	}
+      $sorted =  $transactionsCollection->sortByDesc([
+		   fn (array $transacArray1, array $transacArray2) 
+			=> $transacArray1['accruedInterest_amount'] <=> $transacArray2['accruedInterest_amount']
+		]);		
+	  
+      return $sorted->values()->all();
+   }
 
-	public function sortTransactionsByTypeCountDesc(string $securitiesAccountId) : ?Collection
-    {		
-		$accountId = AccountId::from($securitiesAccountId);
+   public function sortTransactionsByTypeCountDesc(string $securitiesAccountId) : ?Collection
+   {		
+     $accountId = AccountId::from($securitiesAccountId);
 		
-		$securitiesAccountId = $accountId->securitiesAccountId();
+     $securitiesAccountId = $accountId->securitiesAccountId();
 
-		$unsortedTransactionsCollection = DB::table('transactions')
-												->where('account_id','=',$securitiesAccountId)	
-												->select(DB::raw('count(transactionType_name) as transactionType_count'))	
-												->groupBy('transactionType_name')		
-												->get();
-												
-		if ($unsortedTransactionsCollection->isEmpty()) {
-			return null;
-		}
+     $unsortedTransactionsCollection = DB::table('transactions')
+					   ->where('account_id','=',$securitiesAccountId)	
+					   ->select(DB::raw('count(transactionType_name) as transactionType_count'))	
+					   ->groupBy('transactionType_name')		
+					   ->get();
+	   
+     if ($unsortedTransactionsCollection->isEmpty()) {
+       return null;
+     }
 		
-		if ($unsortedTransactionsCollection->count() == 1) {
-			return $unsortedTransactionsCollection;
-		}
+     if ($unsortedTransactionsCollection->count() == 1) {
+        return $unsortedTransactionsCollection;
+     }
 		
-		$sorted = $unsortedTransactionsCollection->sortByDesc([
-			fn (array $a, array $b) => $a['transactionType_count'] <=> $b['transactionType_count'],
-		]);
+     $sorted = $unsortedTransactionsCollection->sortByDesc([
+                  fn (array $a, array $b) => $a['transactionType_count'] <=> $b['transactionType_count'],
+	       ]);
 
-		return $sorted->values()->all();	
-	}
+     return $sorted->values()->all();	
+  }
 }
