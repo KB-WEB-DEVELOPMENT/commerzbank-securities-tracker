61dabd16eb57d9961246e1ea0b87aa5cbe243a6e
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:42:23 +0200

    Create AccruedInterest.vue

---
+++ resources/js/Pages/Transactions/AccruedInterest.vue
@@ -0,0 +1,56 @@
+<template>
+
+    <Head title="My Security Account - Transactions" />
+
+    <BreezeAuthenticatedLayout>
+	
+        <template #header>			
+            <p>Customer Name: {{ viewModel.user.name }}</p>
+            <p>Customer Email: {{ viewModel.user.email }}</p>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200 grid grid-cols-6 gap-4">
+					
+						<h3>Transactions</h3>
+						
+						<p>Securities Account Id: {{ viewModel.securitiesAccountId }}</p>
+						
+						<br/>
+						
+						<h5>Transactions Summary | Sorted by Accrued interest descending</h5>
+
+							<SortedTransactions />		
+							
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+	</BreezeAuthenticatedLayout>
+	
+</template>
+
+<script>
+
+	import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue'
+	import SortedTransactions from '@/Components/Transaction/TransactionsSortedByAccruedInterestAmount.vue'
+	import { Head } from '@inertiajs/inertia-vue3';
+
+	export default {
+		components: {
+			BreezeAuthenticatedLayout,
+			Head,
+			SortedTransactions,
+		},
+		props: {
+			viewModel: {
+				type: Object,
+				required: true,
+			},
+		}
+	}
+	
+</script>
