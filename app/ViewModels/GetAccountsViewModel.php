c7391f76611a93cca7d1357cd716b89754d95ddb
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:20:44 +0200

    Create GetAccountsViewModel.php

---
+++ app/ViewModels/GetAccountsViewModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\ViewModels;
+
+use App\Models\User;
+
+use Illuminate\Database\Eloquent\Collection;
+
+class GetAccountsViewModel extends ViewModel
+{
+    public function __construct(
+		private User $user,
+		private ?Collection $accountsCollection
+	){}
+
+	public function user(): User
+    {
+        return $this->user;
+    }	
+
+	public function accountsCollection(): ?Collection
+    {
+        return $this->accountsCollection;
+    }
+}
