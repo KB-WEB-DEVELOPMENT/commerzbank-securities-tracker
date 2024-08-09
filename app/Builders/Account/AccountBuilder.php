476eae8b690abd0f09004d00b0d42273de628b98
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 10:53:30 +0200

    Create AccountBuilder.php

---
+++ app/Builders/Account/AccountBuilder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Builders\Acount;
+
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Database\Eloquent\Builder;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Collection;
+
+class AccountBuilder extends Builder
+{   
+	public function securityAccoundIds(): Collection
+	{
+		return DB::table('accounts')
+					->select('securitiesAccountId')
+					->where('user_id',Auth::id())
+					->latest()
+					->get();
+											
+	}	
+}
