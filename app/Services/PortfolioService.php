3e141c838b8983591630c7b9694de70825ff34c1
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:15:26 +0200

    Create PortfolioService.php

---
+++ app/Services/PortfolioService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Commerzbank\DataTransferObjects\PortfolioData;
+use App\Services\Commerzbank\DataTransferObjects\PositionsData;
+
+class PortfolioService
+{
+    public function __construct(
+    ){}
+
+	public function portfolio(): PortfolioData
+    {		
+		$portfolioArray = [];
+		
+		$portfolioArray = include('../../storage/imports/portfolio.php');
+		
+		$portfolioData = PortfolioData::fromArray($portfolioArray);
+
+		return $portfolioData; 
+	}
+	
+
+}	
