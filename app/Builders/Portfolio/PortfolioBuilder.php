f0e7c137c5b80d534107dac2ff6edebdc8ce869a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 10:54:26 +0200

    Create PortfolioBuilder.php

---
+++ app/Builders/Portfolio/PortfolioBuilder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Builders\Acount;
+
+use App\ValueObjects\AccountId;
+
+use App\DataTransferObjects\PortfolioData;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+
+class PortfolioBuilder extends Builder
+{    
+	public function portfolio(string $securitiesAccountId): Collection
+	{
+		$accountId = AccountId::from($securitiesAccountId);
+		
+		$securitiesAccountId = $accountId->securitiesAccountId();
+		
+		return DB::table('portfolios')
+					->where('account_id',$securitiesAccountId)
+					->latest()
+					->get()
+					->map(fn (object $data) => PortfolioData::fromArray((array) $data));					
+	}
+	
+	
+
+}
