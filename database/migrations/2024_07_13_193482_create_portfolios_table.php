e6b4056ed13343646e12671a1bcaf194fd2cce34
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:12:02 +0200

    Update 2024_07_13_193482_create_portfolios_table.php

--- database/migrations/2024_07_13_193482_create_portfolios_table.php
+++ database/migrations/2024_07_13_193482_create_portfolios_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('portfolios', function (Blueprint $table) {
-            
-			$table->id();
-			$table->string('creationDay');
-			$table->string('effectiveDate');
-			$table->decimal('totalValue_amount',total:12,places:2);
-			$table->string('totalValue_currency');
-			$table->foreign('account_id')->references('securitiesAccountId')->on('accounts');
-	    });
+        Schema::create('portfolios', function (Blueprint $table) {    
+	    $table->id();
+	    $table->string('creationDay');
+	    $table->string('effectiveDate');
+	    $table->decimal('totalValue_amount',total:12,places:2);
+	    $table->string('totalValue_currency');
+	    $table->foreign('account_id')->references('securitiesAccountId')->on('accounts');
+	});
     }
 
     /**
