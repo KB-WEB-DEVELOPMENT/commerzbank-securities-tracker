126877fafaf42866b2453da258bcbb88309799f3
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:53:39 +0200

    Update ToTradingDate.php

--- app/ValueObjects/ToTradingDate.php
+++ app/ValueObjects/ToTradingDate.php
@@ -10,15 +10,15 @@ class ToTradingDate
 
     public static function from(string $unformattedDate): self
     {						
-		if (date_create($unformattedDate) == false) {
-			throw new Exception('The value of the variable you provided cannot be converted into a proper date.');	
-		}
-		return new static(date_create($this->unformattedDate));
+        if (date_create($unformattedDate) == false) {
+	   throw new Exception('The value of the variable you provided cannot be converted into a proper date.');	
 	}
+		
+	return new static(date_create($this->unformattedDate));
+    }
 
     public function format(): string
     {
        return date_format($this->unformattedDate,'Y-m-d'); 
     }
-
 }
