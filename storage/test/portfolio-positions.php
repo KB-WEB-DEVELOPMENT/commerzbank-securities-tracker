644a696ef601df5035f8ce0a5c4277512aed235a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:57:25 +0200

    Add files via upload

---
+++ storage/test/portfolio-positions.php
@@ -0,0 +1,53 @@
+<?php
+ return [
+    "positions" => [
+        [
+            "accruedInterest" => [
+                "amount" => 25.73,
+                "currency" => "EUR",
+            ],
+            "currentPrice" => [
+                "price" => [
+                    "amount" => 25.73,
+                    "unit" => "EUR",
+                ],
+                "exchangeRate" => 1,
+                "exchangeRateDate" => "2019-02-20",
+                "quoteDate" => "2019-02-20",
+            ],
+            "initialPrice" => [
+                "price" => [
+                    "amount" => 25.73,
+                    "unit" => "EUR",
+                ],
+                "initialExchangeRate" => 1,
+            ],
+            "lastTradeDate" => "2019-02-20",
+            "payedAccruedInterest" => [
+                "amount" => 25.73,
+                "currency" => "EUR",
+            ],
+            "payout" => [
+                "amount" => 25.73,
+                "currency" => "EUR",
+            ],
+            "positionId" =>  "349320001000_PORTFOLIO_DE000CBK1001_004_0000000000000",
+            "quantity" => [
+                "amount" => 25.73,
+                "unit" => "EUR",
+            ],
+            "masterdata" => [
+                "securitiesMasterdata" => [
+                    "isin" => "DE0005190003",
+                    "wkn" => "519000",
+                    "name" => "Commerzbank AG",
+                    "notationType" => "Piece",
+                ],
+                "currency" => "EUR",
+                "maturityDate" => "2019-02-20",
+                "vote" => "Sell",
+            ],
+		],
+	],
+];
+?>
---
+++ storage/test/transactions.php
@@ -0,0 +1,86 @@
+<?php
+	return [
+		"transactions" => [
+			[
+				"accruedInterest" => [
+					"amount" => 25.73,
+					"currency" => "EUR",
+				],
+				"blockInfo" => [
+					"blockText" => "Minimum holding period of 24 months without exemption limit",
+					"blockTo" => "2019-09-11",
+				],
+				"bookingDate" => "2019-02-20",
+				"cancellationInfo" => [
+					"cancelledTransactionId" => "00000000000000000001",
+					"isCancelation" => true,
+				],
+				"costs" => [
+					[
+						"costDescription" => "Provision",
+						"money" => [
+							"amount" => 25.73,
+							"currency" => "EUR",
+						],
+					],
+				],
+				"depository" => "Girosammel",
+				"exchangeRate" => 1,
+				"positionId" =>
+					"349320001000_PORTFOLIO_DE000CBK1001_004_0000000000000",
+				"price" => [
+					"amount" => 25.73,
+					"unit" => "EUR",
+				],
+				"masterdata" => [
+					"isin" => "DE0005190003",
+					"wkn" => "519000",
+					"name" => "Commerzbank AG",
+					"notationType" => "Piece",
+				],
+				"size" => [
+					"amount" => 150,
+					"unit" => "pieces",
+				],
+				"tradingDate" => "2019-02-20",
+				"tradingPlatform" => "Boerse",
+				"tradingTime" => "2021-10-04T10:32:08",
+				"transactionId" => "0000000000000000001",
+				"transactionType" => [
+					"id" => "5",
+					"name" => "PURCHASE",
+				],
+				"valutaDate" => "2019-02-21",
+				"settlementAccount" => "100000000000EUR",
+				"marketValue" => [
+					"amount" => 25.73,
+					"currency" => "EUR",
+				],
+				"actualAmount" => [
+					"amount" => 25.73,
+					"currency" => "EUR",
+				],
+				"externalOrderId" => "00001",
+				"settlementNumber" => "+001000012",
+				"executionNumber" => 1,
+				"clientOrderNumber" => "D0000000000000OM",
+				"transactionDetailedType" => "PURCHASE_CLEARING",
+				"taxDetails" => [
+					"taxes" => [
+						[
+							"taxType" => "10005",
+							"taxTypeDescription" => [
+								"de" => "Kapitalertragsteuer",
+								"en" => "Capital yield tax",
+							],
+							"amount" => [
+								"amount" => 25.73,
+								"currency" => "EUR",
+							],
+						],
+					],
+				],
+			],
+		],
+	];
+?>	
