72a120da6c237a46301588793310c09e1e5ee28a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 14:06:58 +0200

    Update HttpStatusCode.php

--- app/Enums/HttpStatusCode.php
+++ app/Enums/HttpStatusCode.php
@@ -4,10 +4,10 @@ namespace App\Enums;
 
 enum HttpStatusCode: int
 {
-	case OK = 200;
-	case BAD_REQUEST = 400;
-	case UNAUTHORIZED = 401;
-	case FORBIDDEN = 403;
-	case NOT_FOUND = 404;
-	case INTERNAL_ERROR = 500;
+   case OK = 200;
+   case BAD_REQUEST = 400;
+   case UNAUTHORIZED = 401;
+   case FORBIDDEN = 403;
+   case NOT_FOUND = 404;
+   case INTERNAL_ERROR = 500;
 }
