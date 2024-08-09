5dbef498daa39f614c6894fd69efdb5b1f76de98
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 13:47:02 +0200

    Update TaxData.php

--- app/DataTransferObjects/TaxData.php
+++ app/DataTransferObjects/TaxData.php
@@ -6,12 +6,12 @@ use Spatie\LaravelData\Data;
 
 class TaxData extends Data 
 {					
-	public function __construct(
-		private readonly string $taxType,
-		private readonly string $taxTypeDescription_de,
-		private readonly string $taxTypeDescription_en,
-		#[Min(0)]
-		private readonly float  $amount_amount,
-		private readonly string $amount_currency,
-    ) {}
+   public function __construct(
+      private readonly string $taxType,
+      private readonly string $taxTypeDescription_de,
+      private readonly string $taxTypeDescription_en,
+      #[Min(0)]
+      private readonly float  $amount_amount,
+      private readonly string $amount_currency,
+   ) {}
 }
