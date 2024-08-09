e4ed8b391cb19a6aa5697205bfbab5ea738dbacd
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 14:07:25 +0200

    Update TransactionTypeName.php

--- app/Enums/TransactionTypeName.php
+++ app/Enums/TransactionTypeName.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum TransactionTypeName: string
 {
-	case PURCHASE = 'purchase';
-	case SALE = 'sale';
-	case DELIVERY = 'delivery';
-	case DEPOSIT = 'deposit';
-	case MATURITY = 'maturity';
+  case PURCHASE = 'purchase';
+  case SALE = 'sale';
+  case DELIVERY = 'delivery';
+  case DEPOSIT = 'deposit';
+  case MATURITY = 'maturity';
 }
