30ef020b6bfeba1a72b598dd481d9d2861bbd089
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:15:40 +0200

    Add files via upload

---
+++ app/Services/TransactionsService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Commerzbank\DataTransferObjects\TransactionsData;
+
+class TransactionsService
+{
+    public function __construct(
+    ){}
+
+	public function transactions(): TransactionsData
+    {		
+		$transactionsArray  = [];
+		
+		$transactionsArray = include('../../storage/imports/transactions.php');
+		
+		$transactionsData = TransactionsData::fromArray($transactionsArray);
+
+		return $transactionsData; 
+	}
+}	
