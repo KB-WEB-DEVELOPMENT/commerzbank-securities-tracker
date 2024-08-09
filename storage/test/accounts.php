1618fb7b7518838179643d90799a64107686dcb0
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:57:00 +0200

    Create accounts.php

---
+++ storage/test/accounts.php
@@ -0,0 +1,51 @@
+<?php
+	return [
+		"listFiltered" => "y",
+		"securitiesAccountIds" => [
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001000",
+				"securitiesAccountId" => "349320001000",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001001",
+				"securitiesAccountId" => "349320001001",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001002",
+				"securitiesAccountId" => "349320001002",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001003",
+				"securitiesAccountId" => "349320001003",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001004",
+				"securitiesAccountId" => "349320001004",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001005",
+				"securitiesAccountId" => "349320001005",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001006",
+				"securitiesAccountId" => "349320001006",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001007",
+				"securitiesAccountId" => "349320001007",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001008",
+				"securitiesAccountId" => "349320001008",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_34932000109",
+				"securitiesAccountId" => "349320001009",
+			],
+			[
+				"pseudonymizedAccountId" => "pseudomized_349320001010",
+				"securitiesAccountId" => "349320001010",
+			],			
+		],
+	];
+?>
