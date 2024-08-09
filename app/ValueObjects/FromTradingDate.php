9064f4d071de75c3dd52b3e2b28ec680e34e3c26
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:52:19 +0200

    Update FromTradingDate.php

--- app/ValueObjects/FromTradingDate.php
+++ app/ValueObjects/FromTradingDate.php
@@ -5,20 +5,20 @@ namespace App\ValueObjects;
 class FromTradingDate
 {
     public function __construct(
-		private readonly DateTime $unformattedDate
-	){}
+      private readonly DateTime $unformattedDate
+    ){}
 
     public static function from(string $unformattedDate): self
     {						
-		if (date_create($unformattedDate) == false) {
-			throw new Exception('The value of the variable you provided cannot be converted into a proper date.');	
-		}
-		return new static(date_create($this->unformattedDate));
+       if (date_create($unformattedDate) == false) {
+	  throw new Exception('The value of the variable you provided cannot be converted into a proper date.');	
 	}
+	
+	return new static(date_create($this->unformattedDate));
+    }
 
     public function format(): string
     {
        return date_format($this->unformattedDate,'Y-m-d'); 
     }
-
 }
