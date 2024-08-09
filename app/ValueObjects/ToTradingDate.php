1aae255176bdd00afb549d57cb1ecc705714d14a
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:50:59 +0200

    Add files via upload

---
+++ app/ValueObjects/ToTradingDate.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\ValueObjects;
+
+class ToTradingDate
+{
+    public function __construct(
+		private readonly DateTime $unformattedDate
+	){}
+
+    public static function from(string $unformattedDate): self
+    {						
+		if (date_create($unformattedDate) == false) {
+			throw new Exception('The value of the variable you provided cannot be converted into a proper date.');	
+		}
+		return new static(date_create($this->unformattedDate));
+	}
+
+    public function format(): string
+    {
+       return date_format($this->unformattedDate,'Y-m-d'); 
+    }
+
+}
