4391a82989a630813833c8e6fcaebc7f3aee9bde
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 14:57:24 +0200

    Update AccountId.php

--- app/ValueObjects/AccountId.php
+++ app/ValueObjects/AccountId.php
@@ -5,24 +5,23 @@ namespace App\ValueObjects;
 class AccountId
 {
     public function __construct(
-		private readonly string $securitiesAccountId,
-	){}
+        private readonly string $securitiesAccountId,
+    ){}
 
     public static function from(string $securitiesAccountId): self
     {
-		$pattern = "/^[0-9]{12,13}$/";
-		$found = preg_match($pattern,$securitiesAccountId);
+        $pattern = "/^[0-9]{12,13}$/";
+        $found = preg_match($pattern,$securitiesAccountId);
 
-		if (!$found) {
-			throw new Exception('Your $securitiesAccountId variable format does not match the format Commerzbank expects.');
-		}
-
-		return new static($securitiesAccountId);
+        if (!$found) {
+	   throw new Exception('Your $securitiesAccountId variable format does not match the format Commerzbank expects.');
+        }
+	    
+        return new static($securitiesAccountId);
     }
 	
-	public function securitiesAccountId(): string
+    public function securitiesAccountId(): string
     {
        return $this->securitiesAccountId;
     }
 }
-
