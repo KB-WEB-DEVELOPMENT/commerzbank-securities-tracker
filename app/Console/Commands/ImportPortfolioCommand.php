8d997d25ce0f36b91d5c45c43199822f6681e666
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 10:58:54 +0200

    Create ImportPortfolioCommand.php

---
+++ app/Console/Commands/ImportPortfolioCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Services\PortfolioService;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ImportPortfolioCommand extends Command
+{
+    protected $signature = 'portfolio:import';
+    protected $description = 'Oversimplified command example: calls local version of user Commerzbank portfolio API from local 
+	                        array file \storage\imports\portfolio.php (does not call Commerzbank API)';
+
+    public function handle():int
+    {
+        DB::transaction(function () use (PortfolioService $portfolioService) {
+
+			$portfolio = $portfolioService->portfolio();
+						
+			if (!$portfolio) {
+				return self::FAILURE;
+			}
+			
+			/*
+				Next step: store the uploaded $portfolio data with an associated, supplied portfolio id as foreign key
+				in the database
+			*/
+			
+			return self::SUCCESS;	
+        });
+    }
+}
