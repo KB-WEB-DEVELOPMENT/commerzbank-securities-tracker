fce1442e2927f946d944d207545236fe56c07e84
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:04:27 +0200

    Update PortfolioFactory.php

--- database/factories/PortfolioFactory.php
+++ database/factories/PortfolioFactory.php
@@ -11,17 +11,16 @@ class PortfolioFactory extends Factory
 {
     protected $model = Portfolio::class;
 	
-	public function definition():array
+    public function definition():array
     {       
-		$account = Account::factory()->create();
+       $account = Account::factory()->create();
 		
-		return [
-		
-			'creationDay' =>  '2024-01-01',
-			'effectiveDate' => '2024-01-01',
-			'totalValue_amount' => fake()->randomFloat(2,200,2000),
-			'totalValue_currency' => 'EUR',
-			'account_id' => $account->securitiesAccountId,
-		];
+        return [		
+	   'creationDay' =>  '2024-01-01',
+	   'effectiveDate' => '2024-01-01',
+	   'totalValue_amount' => fake()->randomFloat(2,200,2000),
+	   'totalValue_currency' => 'EUR',
+	   'account_id' => $account->securitiesAccountId,
+       ];
     }
 }
