b597ff93ec52dfc9ff3afcf3b257938d7472bbaa
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 10:57:33 +0200

    Create PortfolioCollection.php

---
+++ app/Collections/PortfolioCollection.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Collections;
+
+use App\ValueObjects\AccountId;
+
+use App\Models\Portfolio;
+use App\Models\Position;
+
+use Illuminate\Database\Eloquent\Collection;
+
+class PortfolioCollection extends Collection
+{
+  public function averagePayoutPerPosition(string $securitiesAccountId) : float
+  {		
+		$accountId = AccountId::from($securitiesAccountId);
+		
+		$securitiesAccountId = $accountId->securitiesAccountId();
+	
+		$portfolio = Portfolio::where('securitiesAccountId',$securitiesAccountId)->first();
+  
+		if (!$portfolio) {
+				return 0;
+		}
+
+		$positions = $portfolio->positions;
+
+		if (!$positions) {
+				return 0;
+		}		
+  
+		$positionsCollection = ($positions instanceof Collection) ? $positions : collect($positions);
+  
+		// for demonstration purposes, we assume all payouts are in the same currency
+		$totalPayoutsAmount = (float)$positionsCollection->sum('payout_amount'); 
+  
+		$PayoutsCount = (int)$positionsCollection->count();
+  
+		$averagePayoutPerPosition = (float)round($totalPayoutsAmount / $PayoutsCount,2);
+		
+		return $averagePayoutPerPosition;
+  }
+
+}
