cf8b416195791d21bf3effc26e11ff5d130036cf
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 11:59:06 +0200

    Create GetAccountsTest.php

---
+++ tests/Feature/Accounts/GetAccountsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Accounts;
+
+use App\Models\User;
+use App\Models\Account;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class GetAccountsTest
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_should_display_the_security_accounts():void
+    {
+        $user = User::factory()->create();
+        
+		$this->actingAs($user);
+		
+		$account = Account::factory()->create([
+            'user_id' => $user->id,
+        ]);
+		
+		$securitiesAccountId = $account->securitiesAccountId;
+		
+		$response = $this->get('/accounts');
+     
+        $response->assertSee('Access your portfolio for each of your security acccounts')->assertSee($securityAccountId);
+    }
+}
