8ace9f57152ddd991f61b6dc923c275c49a86432
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-09 15:35:56 +0200

    Update GetTransactionsTest.php

--- tests/Feature/Transactions/GetTransactionsTest.php
+++ tests/Feature/Transactions/GetTransactionsTest.php
@@ -17,55 +17,54 @@ class GetTransactionsTest
     {
         $user = User::factory()->create();
 		
-		$this->actingAs($user);
+	$this->actingAs($user);
 		
-		$account = Account::factory()->create([
+	$account = Account::factory()->create([
             'user_id' => $user->id,
         ]);
 		
-		$transaction1 = Transaction::factory()->create([
+	$transaction1 = Transaction::factory()->create([
             'account_id' => $account->securitiesAccountId,
         ]);
 		
-		$transaction2 = Transaction::factory()->create([
+	$transaction2 = Transaction::factory()->create([
             'account_id' => $account->securitiesAccountId,
         ]);
 		
-		$transaction3 = Transaction::factory()->create([
+	$transaction3 = Transaction::factory()->create([
             'account_id' => $account->securitiesAccountId,
         ]);
 		
-		$booking_date3 = $transaction3->bookingDate;
+	$booking_date3 = $transaction3->bookingDate;
 		
-		$transactions_url = '/accounts/' . $account->securitiesAccountId . '/transactions';
+	$transactions_url = '/accounts/' . $account->securitiesAccountId . '/transactions';
 				
-		$response = $this->get($transactions_url);	
+	$response = $this->get($transactions_url);	
 
-		$response->assertSee('Transactions Summary')->assertSee($booking_date3);
+	$response->assertSee('Transactions Summary')->assertSee($booking_date3);
 		
     }
 	
-	public function it_should_display_the_transaction_details():void
-    {
+     public function it_should_display_the_transaction_details():void
+     {
         $user = User::factory()->create();
 		
-		$this->actingAs($user);
+	$this->actingAs($user);
 		
-		$account = Account::factory()->create([
+	$account = Account::factory()->create([
             'user_id' => $user->id,
         ]);
 		
-		$transaction = Transaction::factory()->create([
+	$transaction = Transaction::factory()->create([
             'account_id' => $account->securitiesAccountId,
         ]);
 		
-		$isin_string = 'ISIN: ' . $transaction->masterdata_isin;
+	$isin_string = 'ISIN: ' . $transaction->masterdata_isin;
 		
-		$transaction_url = '/accounts/' . $account->securitiesAccountId . '/transactions/' . $transaction->id;
+	$transaction_url = '/accounts/' . $account->securitiesAccountId . '/transactions/' . $transaction->id;
 				
-		$response = $this->get($transaction_url);		
+	$response = $this->get($transaction_url);		
 		
-		$response->assertSee('Transaction Details')->assertSee($isin_string);
-		
-    }
+	$response->assertSee('Transaction Details')->assertSee($isin_string);	
+     }
 }
