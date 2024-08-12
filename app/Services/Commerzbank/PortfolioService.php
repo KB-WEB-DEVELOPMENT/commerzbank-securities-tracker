7526e93a83a4805c83710175fb64028b2cbe11df
Kami Barut-Wanayo <kamibarut@yahoo.com>
2024-08-12 18:25:10 +0200

    Update PortfolioService.php

--- app/Services/Commerzbank/PortfolioService.php
+++ app/Services/Commerzbank/PortfolioService.php
@@ -15,56 +15,55 @@ use App\Models\User;
 class PortfolioService
 {
     public function __construct(
-		private readonly string $access_token,
-		private readonly string $uri,
-		private readonly int $timeout
+	private readonly string $access_token,
+	private readonly string $uri,
+	private readonly int $timeout
     ){}
 
-	public function portfolioAssetsOverview(string $securitiesAccountId,FromTradingDate $effectiveTradingDate): PortfolioData|void
+    public function portfolioAssetsOverview(string $securitiesAccountId,FromTradingDate $effectiveTradingDate): PortfolioData|void
     {
-		// note: the auth middleware checks that the user is logged in.	
+       // note: the auth middleware checks that the user is logged in.	
 
-		$accountId = AccountId::from($securitiesAccountId);
+       $accountId = AccountId::from($securitiesAccountId);
 		
-		$securitiesAccountId = $accountId->securitiesAccountId();
+       $securitiesAccountId = $accountId->securitiesAccountId();
 		
-		$user = Auth::user();
+       $user = Auth::user();
 
-		$userAccounts = [];
+       $userAccounts = [];
 		
-		$userAccounts = (array)$user->accounts;
+       $userAccounts = (array)$user->accounts;
 		
-		$securitiesAccountIds = [];
+       $securitiesAccountIds = [];
 		
-		foreach ($userAccounts as $userAccount) {
-			$securitiesAccountIds[] = $userAccount->securitiesAccountId;	
-		}
+       foreach ($userAccounts as $userAccount) {
+	    $securitiesAccountIds[] = $userAccount->securitiesAccountId;	
+       }
 
-		if (!in_array($securitiesAccountId,$securitiesAccountIds)) {
-			return redirect()->route('dashboard');
-		}		
+       if (!in_array($securitiesAccountId,$securitiesAccountIds)) {
+	  return redirect()->route('dashboard');
+       }		
 												
-		$json_url = $this->uri;	
+       $json_url = $this->uri;	
 		
-		$json_url .= $securitiesAccountId . '/portfolio?effectiveDate=' . $effectiveTradingDate->format();
+       $json_url .= $securitiesAccountId . '/portfolio?effectiveDate=' . $effectiveTradingDate->format();
 	    
-		$options =  array(
-						'http' => array(
-									'method' => 'GET',
-									'header' => 'Authorization: Bearer ' . $this->access_token,
-									'timeout' => $this->timeout
-								),
-					);		
+       $options =  array(
+		     'http' => array(
+				 'method' => 'GET',
+				 'header' => 'Authorization: Bearer ' . $this->access_token,
+				  'timeout' => $this->timeout
+			       ),
+		   );		
     
-		$context = stream_context_create($options);
+       $context = stream_context_create($options);
 		
-		$json = file_get_contents($json_url,false,$context);
+       $json = file_get_contents($json_url,false,$context);
 		
-		$portfolioAssetsOverviewArray = json_decode($json,TRUE);
+       $portfolioAssetsOverviewArray = json_decode($json,TRUE);
 		
-		$portfolioData = PortfolioData::fromArray($portfolioAssetsOverviewArray);
+       $portfolioData = PortfolioData::fromArray($portfolioAssetsOverviewArray);
 		
-		return $portfolioData; 
-	}
-
-}
+       return $portfolioData; 
+    }
+ }
